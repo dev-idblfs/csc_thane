1d2f1486fa88d2cdaf9ca24bbf0d93ffa8d018d1
Divyanshu <53134822+dev-idblfs@users.noreply.github.com>
2020-04-09 04:29:54 +0530

    v2.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,21 +32,38 @@ class _CustomWebView extends State<CustomWebView> {
   Completer<WebViewController> _controller = Completer<WebViewController>();
   final url = 'https://sites.google.com/view/digitaldesh-co-in/home';
 
+  Future<void> _launchInWebViewWithDomStorage(String url) async {
+    if (await canLaunch(url)) {
+      launch(url,
+          forceSafariVC: true,
+          forceWebView: true,
+          enableDomStorage: true,
+          enableJavaScript: true).then((value) => Platform.isIOS ? exit(0) : SystemNavigator.pop(animated: true));
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Widget _launchStatus(BuildContext context, AsyncSnapshot<void> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Error: ${snapshot.error}');
+    } else {
+      return const Text('open');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
-      body: Container(
-        padding: EdgeInsets.only(top: 21.0),
-        child: WebView(
-          initialUrl: url,
-          onWebViewCreated: (WebViewController webViewController) {
-            _controller.complete(webViewController);
-          },
-          javascriptMode: JavascriptMode.unrestricted,
-        ),
-      ),
-    );
+        appBar: null,
+        body: Column(
+          children: <Widget>[
+            const Padding(padding: EdgeInsets.all(16.0)),
+            FutureBuilder<void>(
+                future: _launchInWebViewWithDomStorage(url),
+                builder: _launchStatus),
+          ],
+        ));
   }
 }
 
